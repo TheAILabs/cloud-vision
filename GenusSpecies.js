4721b05dd330840ef16065912ac793b1fa10615d
Satyajit <ammesatyajit@gmail.com>
2018-01-07 19:43:25 -0800

    More training data

---
+++ GenusSpecies.js
@@ -0,0 +1,20 @@
+file.read(./datasource-1.html);
+
+for (index = 0, len = a.length; index < len; ++index) {
+    console.log(a[index]);
+
+    if <img src =...> == true {
+    	document.write(find("/...".content));
+    }
+}
+
+var alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
+				 "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+if <em> =...> == true {
+	document.write(find("<em>".content));
+
+}else {
+	document.write(find(">"))
+}
+
